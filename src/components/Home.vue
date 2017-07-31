8588ce82096a995ec2a5338e9fc76db5307c4700
wplay <838523799@qq.com>
2017-07-31 23:18:55 +0800

    first operate and commit

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="right-content">
+		<header>
+			<div class="block">
+				<span class="demonstration">默认</span>
+				<el-date-picker
+					v-model="value1"
+					type="date"
+					placeholder="选择日期"
+					:picker-options="pickerOptions0">
+				</el-date-picker>
+			</div>
+		</header>
+	</div>
+</template>
+
+<style>
+	#right-content {
+		width: 100%;
+		height: 300px;
+		background: cyan;
+	}
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      pickerOptions0: {
+        disabledDate (time) {
+          return time.getTime() < Date.now() - 8.64e7
+        }
+      },
+      value1: ''
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import elementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 
 Vue.config.productionTip = false
+
+Vue.use(elementUI)
 
 /* eslint-disable no-new */
 new Vue({
